48b0c888744d3415a6432c30185a781defe51fe9
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-08-24 13:33:27 -0400

    week 8 updates

---
+++ week-8/Rivas_Calculator.py
@@ -0,0 +1,26 @@
+#
+# ======================================================
+# Title: Python Calculator
+# Author: Professor Krasso
+# Modified by: Rhonda Rivas
+# Date:   8-24-2020
+# Description: Python basic calculator 8.3
+#=====================================================
+#
+#this adds and returns sum of 2 numbers
+def add(num1, num2):
+    return(num1 + num2)
+
+print(add(1, 2))
+
+#this subtracts one number from another & returns the value
+def subtract(num3, num4):
+    return(num3-num4)
+
+print(subtract(4,1))
+
+#this divides one number by another & returns the value
+def divide(num5, num6):
+    return(num5/num6)
+
+print(divide(8,2))
---
+++ week-8/Rivas_hello_world.py
@@ -0,0 +1,10 @@
+#
+# ======================================================
+# Title:  Hello World
+# Author: Professor Krasso
+# Modified by: Rhonda Rivas
+# Date:   8-24-2020
+# Description: Python Hello World 8.2
+#=====================================================
+#
+print('Hello World')
